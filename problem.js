79fa90279c330653685c0182fd6f02521076e0dc
zituhossain5 <zituhossain@gmail.com>
2024-09-12 17:43:44 +0600

    problem 2

--- problem.js
+++ problem.js
@@ -1,3 +1,5 @@
+// First Repeating Character
+
 function repeatChar(str) {
   let firstChar = str.charAt(0);
 
@@ -13,3 +15,19 @@ function repeatChar(str) {
 }
 
 console.log(repeatChar("abbacddac"));
+
+// First Non-repeating Character
+
+function nonRepeatChar(str) {
+  let firstChar = str.charAt(0);
+  let nonRep = "";
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] == firstChar) {
+      str.slice(i, str[i].length);
+      nonRep = nonRep + firstChar;
+    }
+  }
+  return nonRep;
+}
+
+console.log(nonRepeatChar("cccabb"));
